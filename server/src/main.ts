0a0334d301c33ff7e10ba2ee50edbfb82a93bda5
ashenafiDL <ashenafi.debella101@gmail.com>
2023-12-22 20:40:04 +0300

    Add swagger documentation support

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,36 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Add swagger API documentation support
+  const config = new DocumentBuilder()
+    .setTitle('VolunteerLink API endpoints')
+    .setDescription(
+      'Documentation for RESTful API endpoints of VolunteerLink project',
+    )
+    .setVersion('1.0')
+    .setExternalDoc('Postman Collection', '/api-json')
+    .build();
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT);
 }
-bootstrap();
+bootstrap().then(() => {
+  const logger = new Logger();
+  logger.verbose(`Server Available at: http://localhost:${PORT}`);
+  logger.verbose(`API docs available at http://localhost:${PORT}/api`);
+});
